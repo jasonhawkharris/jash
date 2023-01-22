0c8e8d9c53efebf64507ccbafe3822f83cb5b7a4
Jason Hawk Harris <jasonhawkharris@gmail.com>
2023-01-22 17:31:37 -0600

    blueprint for commands

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
-func main()  {
-    fmt.Println("JASH")
+func main() {
+	args := os.Args
+    cmd := args[1]
+
+    switch cmd {
+    case "spent":
+        fmt.Printf("Added new purchase of %s to %s category\n", args[2], args[3])
+    case "made":
+        fmt.Printf("Added new income (%s) from %s\n", args[2], args[3])
+    case "create":
+        fmt.Printf("Added new category (%s)\n", args[2])
+    case "account":
+        fmt.Println("Listing your purchases")
+    default:
+        fmt.Println("Unknown command")
+    }
 }
